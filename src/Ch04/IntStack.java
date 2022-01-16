04ea330ccc3687eefb271017e0c36af576f98b70
junhuyk96 <junhuyk96@gmail.com>
2022-01-16 23:52:52 +0900

    'commit'

---
+++ src/Ch04/IntStack.java
@@ -0,0 +1,40 @@
+package Ch04;
+
+public class IntStack {
+    private int max;
+    private int ptr;
+    private int[] stk;
+
+    public class EmptyIntStackException extends RuntimeException {
+        public EmptyIntStackException() {}
+    }
+
+    public class OverflowIntStackException extends RuntimeException {
+        public OverflowIntStackException() {}
+    }
+
+    public IntStack(int capacity) {
+        ptr = 0;
+        max = capacity;
+        try {
+            stk = new int[max];
+        } catch(OutOfMemoryError e) {
+            max = 0;
+        }
+    }
+
+    public int push(int x) throws OverflowIntStackException {
+        if(ptr >= max) throw new OverflowIntStackException();
+        return stk[ptr++] = x;
+    }
+
+    public int pop(int x) throws EmptyIntStackException {
+        if(ptr <= 0) throw new EmptyIntStackException();
+        return stk[--ptr];
+    }
+
+    public int peek(int x) throws EmptyIntStackException {
+        if(ptr <= 0 ) throw new EmptyIntStackException();
+        return stk[ptr - 1];
+    }
+}
